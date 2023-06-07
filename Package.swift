751eb8244f643a42a821d37bcbb1ccdffac7cea5
Óscar Morales Vivó <oscarmv@mac.com>
2023-06-07 15:22:48 -0700

    Shrunk Package.swift to only contain `NetworkDependency` stuff.

--- Package.swift
+++ Package.swift
@@ -4,55 +4,31 @@
 import PackageDescription
 
 let package = Package(
-    name: "MiniDepIn",
-    platforms: [ // We require Combine for some of the dependency micropackages so that limits what we support.
-        .iOS(.v14),
-        .macCatalyst(.v14),
-        .macOS(.v11),
-        .tvOS(.v14),
-        .watchOS(.v7)
+    name: "NetworkDependency",
+    platforms: [
+        .iOS(.v11),
+        .macCatalyst(.v13),
+        .macOS(.v10_13),
+        .tvOS(.v11),
+        .watchOS(.v4)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "MiniDePin",
-            targets: ["MiniDePin"]
-        ),
-        .library(
-            name: "LocationDependency",
-            targets: ["LocationDependency"]
-        ),
         .library(
             name: "NetworkDependency",
             targets: ["NetworkDependency"]
         )
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Gabardone/SwiftUX", from: "0.0.2-alpha")
+        .package(url: "https://github.com/Gabardone/GlobalDependencies", from: "2.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MiniDePin",
-            dependencies: []
-        ),
-        .testTarget(
-            name: "MiniDePinTests",
-            dependencies: ["MiniDePin"]
-        ),
-        .target(
-            name: "LocationDependency",
-            dependencies: ["MiniDePin", "SwiftUX"]
-        ),
-        .testTarget(
-            name: "LocationDependencyTests",
-            dependencies: ["MiniDePin", "LocationDependency", "SwiftUX"]
-        ),
         .target(
             name: "NetworkDependency",
-            dependencies: ["MiniDePin"]
+            dependencies: ["GlobalDependencies"]
+        )
+        .testTarget(
+            name: "NetworkDependencyTests",
+            dependencies: ["GlobalDependencies"]
         )
     ]
 )
