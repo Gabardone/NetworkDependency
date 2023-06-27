ff2cfca5a1dc4816cfe3cdbe52e6c5535bd3229e
Óscar Morales Vivó <oscarmv@mac.com>
2023-06-26 21:20:12 -0700

    Made MockNetwork into ComposableNetwork

---
+++ Sources/NetworkDependency/ComposableNetwork.swift
@@ -0,0 +1,28 @@
+//
+//  ComposableNetwork.swift
+//
+//
+//  Created by Óscar Morales Vivó on 5/28/23.
+//
+
+import Foundation
+
+/**
+ A simple value type that implements `Network` through functional injection.
+
+ Use this to build custom implementations of `Network` that can be simply expressed with an encapsulated action.
+ Awfully convenient for test mocking as well.
+ */
+public struct ComposableNetwork {
+    public init(dataForURLOverride: @escaping (URL) async throws -> Data) {
+        self.dataForURLOverride = dataForURLOverride
+    }
+
+    var dataForURLOverride: (URL) async throws -> Data
+}
+
+extension ComposableNetwork: Network {
+    public func dataFor(url: URL) async throws -> Data {
+        try await dataForURLOverride(url)
+    }
+}
